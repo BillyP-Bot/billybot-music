a0ae95ea653287e3a81bb85650fa172fb3756596
Chris Leveille <cdleveille@gmail.com>
2025-04-08 18:42:19 -0400

    gonk init

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-
-export const Config = {
-	IS_PROD: process.env.NODE_ENV === "production",
-	BOT_TOKEN: process.env.BOT_TOKEN
-};
-
-if (!Config.BOT_TOKEN) throw Error("BOT_TOKEN env var has not been set");
